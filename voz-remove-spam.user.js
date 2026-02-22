5770acddee99f0066d2c88edc562ea16a28d824f
Fioren <102145692+FiorenMas@users.noreply.github.com>
2026-02-22 11:19:16 +0700

    Update voz-remove-spam.user.js

--- voz-remove-spam.user.js
+++ voz-remove-spam.user.js
@@ -7,6 +7,8 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=voz.vn
 // @grant        none
 // @run-at       document-idle
+// @downloadURL  https://raw.githubusercontent.com/FiorenMas/My-userscript/refs/heads/main/voz-remove-spam.user.js
+// @updateURL    https://raw.githubusercontent.com/FiorenMas/My-userscript/refs/heads/main/voz-remove-spam.user.js
 // ==/UserScript==
 
 (function () {
